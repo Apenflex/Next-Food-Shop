9f5a5e79bc22d43ce3b061c824c4c68eb58fa8df
Svyatoslav <blackdbf@gmail.com>
2023-06-04 21:23:07 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,22 +5,22 @@ import { cookies } from 'next/headers'
 import { NavBar } from '@/components'
 import ErrorBoundary from '@/components/ErrorBoundary'
 import { delay } from '@/lib/async'
-import { getUserFromCookie } from '@/lib/auth'
 
+// import { getUserFromCookie } from '@/lib/auth'
 import QueryWrapper from './QueryWrapper'
 export const metadata = {
     title: 'Burger Shop',
     description: 'Burger Shop is a demo app built with Next.js and Tailwind CSS.',
 }
 
-const getData = async () => {
-    await delay(1500)
-    const user = await getUserFromCookie(cookies())
-    return user
-}
+// const getData = async () => {
+//     await delay(1500)
+//     const user = await getUserFromCookie(cookies())
+//     return user
+// }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-    const user = await getData()
+    // const user = await getData()
     // console.log('render layout')
     return (
         <html lang="en">
@@ -28,7 +28,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
                 <ErrorBoundary>
                     <QueryWrapper>
                         <header>
-                            <NavBar user={user?.name} />
+                            <NavBar
+                                user={user?.name}
+                            />
                         </header>
                         <main className="container mx-auto">{children}</main>
                     </QueryWrapper>
