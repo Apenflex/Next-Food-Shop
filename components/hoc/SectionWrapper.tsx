62529a0afde1dd6106449873564f41243d1dfd42
Svyatoslav <blackdbf@gmail.com>
2023-06-12 17:57:10 +0300

    fix: user authentication

--- components/hoc/SectionWrapper.tsx
+++ components/hoc/SectionWrapper.tsx
@@ -3,10 +3,21 @@ import { FunctionComponent } from 'react'
 
 import { styles } from '@/components/styles'
 
-const SectionWrapper = (Component: FunctionComponent) =>
+/**
+ * Wraps the provided component with a section wrapper.
+ *
+ * @param {FunctionComponent} Component - The component to be wrapped.
+ * @param {string} idName - The ID attribute for the section wrapper.
+ * @returns {JSX.Element} - The wrapped component within a section wrapper.
+ */
+const SectionWrapper = (Component: FunctionComponent, idName: string) =>
     function HOC() {
         return (
-            <section className={`${styles['padding']} max-w-7xl mx-auto relative z-0 h-screen`}>
+            <section className={`${styles['padding']} max-w-7xl mx-auto relative z-0`}>
+                <span className="hash-span" id={idName}>
+                    &nbsp;
+                </span>
+
                 <Component />
             </section>
         )
